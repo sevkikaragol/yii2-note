d01c59f6c2d92c6bce61819f5ab4c634d7d41ff7
Sevki KARAGOL <sevkaragol@gmail.com>
2021-01-02 11:34:02 +0000

    Update

---
+++ src/models/Not.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace note\src\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "not".
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property string|null $body
+ */
+class Not extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'not';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['body'], 'string'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'body' => 'Body',
+        ];
+    }
+}
